e771997873f84eb79a2ecbe3864c97ce844e1cca
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-11-16 20:31:37 +0530

    connecting fpc with gatwayAPI

--- client_sdk/test-GatewayAPI/app.go
+++ client_sdk/test-GatewayAPI/app.go
@@ -1,12 +1,82 @@
+// // package main
+
+// // import (
+// // 	"fmt"
+// // 	"net/http"
+
+// // 	"github.com/hyperledger/fabric-gateway/pkg/client"
+// // 	"github.com/hyperledger/fabric-gateway/pkg/identity"
+// // 	// "github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+// // )
+
+// // type OrgSetup struct {
+// // 	OrgName      string
+// // 	MSPID        string
+// // 	CryptoPath   string
+// // 	CertPath     string
+// // 	KeyPath      string
+// // 	TLSCertPath  string
+// // 	PeerEndpoint string
+// // 	GatewayPeer  string
+// // 	Gateway      *client.Gateway
+// // }
+
+// // func (setup *OrgSetup) Connect() error {
+// // 	connectionOptions := &client.ConnectionOptions{
+// // 		Identity: identity.NewX509Identity(setup.MSPID, setup.CertPath, setup.KeyPath),
+// // 		Endpoint: setup.GatewayPeer,
+// // 	}
+// // 	gateway, err := client.Connect(connectionOptions)
+// // 	if err != nil {
+// // 		return err
+// // 	}
+// // 	setup.Gateway = gateway
+// // 	return nil
+// // }
+
+// // // func (setup *OrgSetup) Query(w http.ResponseWriter, r *http.Request) {
+// // // 	// Implement the logic for the Query endpoint
+// // // }
+
+// // // func (setup *OrgSetup) Invoke(w http.ResponseWriter, r *http.Request) {
+// // 	// Implement the logic for the Invoke endpoint
+// // // }
+
+// // func Serve(setup *OrgSetup) {
+// // 	http.HandleFunc("/query", setup.Query)
+// // 	http.HandleFunc("/invoke", setup.Invoke)
+// // 	fmt.Println("Listening (http://localhost:3000/)...")
+// // 	if err := http.ListenAndServe(":3000", nil); err != nil {
+// // 		fmt.Println(err)
+// // 	}
+// // }
+
+// // func main() {
+// // 	setup := &OrgSetup{
+// // 		OrgName:      "Org1",
+// // 		MSPID:        "Org1MSP",
+// // 		CryptoPath:   "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp",
+// // 		CertPath:     "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem",
+// // 		KeyPath:      "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore",
+// // 		TLSCertPath:  "crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt",
+// // 		PeerEndpoint: "localhost:7051",
+// // 		GatewayPeer:  "localhost:7051",
+// // 	}
+// // 	if err := setup.Connect(); err != nil {
+// // 		fmt.Println("Failed to connect to Gateway: ", err)
+// // 		return
+// // 	}
+// // 	Serve(setup)
+// // }
+
 // package main
 
 // import (
 // 	"fmt"
-// 	"net/http"
 
 // 	"github.com/hyperledger/fabric-gateway/pkg/client"
 // 	"github.com/hyperledger/fabric-gateway/pkg/identity"
-// 	// "github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+// 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
 // )
 
 // type OrgSetup struct {
@@ -34,21 +104,13 @@
 // 	return nil
 // }
 
-// // func (setup *OrgSetup) Query(w http.ResponseWriter, r *http.Request) {
-// // 	// Implement the logic for the Query endpoint
-// // }
-
-// // func (setup *OrgSetup) Invoke(w http.ResponseWriter, r *http.Request) {
-// 	// Implement the logic for the Invoke endpoint
-// // }
-
-// func Serve(setup *OrgSetup) {
-// 	http.HandleFunc("/query", setup.Query)
-// 	http.HandleFunc("/invoke", setup.Invoke)
-// 	fmt.Println("Listening (http://localhost:3000/)...")
-// 	if err := http.ListenAndServe(":3000", nil); err != nil {
-// 		fmt.Println(err)
+// func (setup *OrgSetup) GetContract(chaincodeID string) (gateway.Contract, error) {
+// 	network, err := setup.Gateway.GetNetwork("mychannel")
+// 	if err != nil {
+// 		return err
 // 	}
+// 	contract := network.GetContract(chaincodeID)
+// 	return contract, nil
 // }
 
 // func main() {
@@ -66,72 +128,10 @@
 // 		fmt.Println("Failed to connect to Gateway: ", err)
 // 		return
 // 	}
-// 	Serve(setup)
+// 	contract, err := setup.GetContract("mychaincode")
+// 	if err != nil {
+// 		fmt.Println("Failed to get contract: ", err)
+// 		return
+// 	}
+// 	// Now you can use the contract to submit transactions and evaluate queries
 // }
-
-package main
-
-import (
-	"fmt"
-
-	"github.com/hyperledger/fabric-gateway/pkg/client"
-	"github.com/hyperledger/fabric-gateway/pkg/identity"
-	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
-)
-
-type OrgSetup struct {
-	OrgName      string
-	MSPID        string
-	CryptoPath   string
-	CertPath     string
-	KeyPath      string
-	TLSCertPath  string
-	PeerEndpoint string
-	GatewayPeer  string
-	Gateway      *client.Gateway
-}
-
-func (setup *OrgSetup) Connect() error {
-	connectionOptions := &client.ConnectionOptions{
-		Identity: identity.NewX509Identity(setup.MSPID, setup.CertPath, setup.KeyPath),
-		Endpoint: setup.GatewayPeer,
-	}
-	gateway, err := client.Connect(connectionOptions)
-	if err != nil {
-		return err
-	}
-	setup.Gateway = gateway
-	return nil
-}
-
-func (setup *OrgSetup) GetContract(chaincodeID string) (gateway.Contract, error) {
-	network, err := setup.Gateway.GetNetwork("mychannel")
-	if err != nil {
-		return err
-	}
-	contract := network.GetContract(chaincodeID)
-	return contract, nil
-}
-
-func main() {
-	setup := &OrgSetup{
-		OrgName:      "Org1",
-		MSPID:        "Org1MSP",
-		CryptoPath:   "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp",
-		CertPath:     "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem",
-		KeyPath:      "crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore",
-		TLSCertPath:  "crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt",
-		PeerEndpoint: "localhost:7051",
-		GatewayPeer:  "localhost:7051",
-	}
-	if err := setup.Connect(); err != nil {
-		fmt.Println("Failed to connect to Gateway: ", err)
-		return
-	}
-	contract, err := setup.GetContract("mychaincode")
-	if err != nil {
-		fmt.Println("Failed to get contract: ", err)
-		return
-	}
-	// Now you can use the contract to submit transactions and evaluate queries
-}
--- client_sdk/test-GatewayAPI/invoke.go
+++ client_sdk/test-GatewayAPI/invoke.go
@@ -1,43 +1,43 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"net/http"
+// import (
+// 	"fmt"
+// 	"net/http"
 
-	"github.com/hyperledger/fabric-gateway/pkg/client"
-)
+// 	"github.com/hyperledger/fabric-gateway/pkg/client"
+// )
 
-// Invoke handles chaincode invoke requests.
-func (setup *OrgSetup) Invoke(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received Invoke request")
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %s", err)
-		return
-	}
-	chainCodeName := r.FormValue("chaincodeid")
-	channelID := r.FormValue("channelid")
-	function := r.FormValue("function")
-	args := r.Form["args"]
-	fmt.Printf("channel: %s, chaincode: %s, function: %s, args: %s\n", channelID, chainCodeName, function, args)
+// // Invoke handles chaincode invoke requests.
+// func (setup *OrgSetup) Invoke(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Received Invoke request")
+// 	if err := r.ParseForm(); err != nil {
+// 		fmt.Fprintf(w, "ParseForm() err: %s", err)
+// 		return
+// 	}
+// 	chainCodeName := r.FormValue("chaincodeid")
+// 	channelID := r.FormValue("channelid")
+// 	function := r.FormValue("function")
+// 	args := r.Form["args"]
+// 	fmt.Printf("channel: %s, chaincode: %s, function: %s, args: %s\n", channelID, chainCodeName, function, args)
 
-	// Convert []string slice to [][]byte slice
-	byteArgs := make([][]byte, len(args))
-	for i, arg := range args {
-		byteArgs[i] = []byte(arg)
-	}
+// 	// Convert []string slice to [][]byte slice
+// 	byteArgs := make([][]byte, len(args))
+// 	for i, arg := range args {
+// 		byteArgs[i] = []byte(arg)
+// 	}
 
-	network := setup.Gateway.GetNetwork(channelID)
-	contract := network.GetContract(chainCodeName)
-	txn, err := contract.NewTransaction(function, client.WithArguments(byteArgs...))
-	if err != nil {
-		fmt.Fprintf(w, "Error creating transaction: %s", err)
-		return
-	}
-	result, err := txn.Submit()
-	if err != nil {
-		fmt.Fprintf(w, "Error submitting transaction: %s", err)
-		return
-	}
-	fmt.Fprintf(w, "Transaction ID : %s Response: %s", txn.TransactionID(), result)
-}
+// 	network := setup.Gateway.GetNetwork(channelID)
+// 	contract := network.GetContract(chainCodeName)
+// 	txn, err := contract.NewTransaction(function, client.WithArguments(byteArgs...))
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Error creating transaction: %s", err)
+// 		return
+// 	}
+// 	result, err := txn.Submit()
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Error submitting transaction: %s", err)
+// 		return
+// 	}
+// 	fmt.Fprintf(w, "Transaction ID : %s Response: %s", txn.TransactionID(), result)
+// }
 
